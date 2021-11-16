547f02f9aca71c25105e09a1375cbad70b4e0403
Your Name <you@example.com>
2021-11-16 20:36:10 +0100

    remove javascript and fix with it the language dropdown menu

--- inc/resume_content.php
+++ inc/resume_content.php
@@ -238,8 +238,4 @@
         </section>
         -->
     </div>
-    <!-- Bootstrap core JS-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- Core theme JS-->
-    <script src="../js/resume.js"></script>
 </div>
